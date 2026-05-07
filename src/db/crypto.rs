a25f759a1fffbddf0ff41ec172f5c67f7e1e8fd2
majiayu000 <1835304752@qq.com>
2026-05-07 12:42:00 +0800

    style: cargo fmt
    Signed-off-by: majiayu000 <1835304752@qq.com>

--- src/db/crypto.rs
+++ src/db/crypto.rs
@@ -65,10 +65,7 @@ where
                         .truncate(true)
                         .open(&key_path)?;
                     use std::os::unix::fs::PermissionsExt;
-                    std::fs::set_permissions(
-                        &key_path,
-                        std::fs::Permissions::from_mode(0o600),
-                    )?;
+                    std::fs::set_permissions(&key_path, std::fs::Permissions::from_mode(0o600))?;
                     Ok::<_, std::io::Error>(file)
                 } else {
                     Err(e)
